1fd9e855bc731c4de688fd74360837af0bf1ed36
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-04-09 17:38:06 -0500

    create engineer class

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,24 +1,33 @@
 const Engineer = require('../lib/Engineer.js')
 
 test('creates an engineer object', () => {
-  const engineer = new Engineer('name');
-
-  // expect(engineer.name).toBe('John');
-  // expect(engineer.id).toEqual(expect.any(Number));
-
-  // expect(engineer.id).toBe(expect.any(Number));
-  // expect(engineer.email).toBe(expect.any(String));
+  const engineer = new Engineer('Mary', 1480, 'mary@email.com', 'MaryGitHub');
+  expect(engineer.name).toBe('Mary');
+  expect(engineer.id).toEqual(expect.any(Number));
+  expect(engineer.email).toStrictEqual(expect.any(String));
+  expect(engineer.github).toEqual(expect.any(String));
 });
 
-// describe('Engineer', () => {
-//   decribe('', () => { 
-//     it('', () => { 
+test("get engineer name", () => {
+  const engineer = new Engineer('Mary', 1480, 'mary@email.com', 'MaryGitHub');
 
-//       const test = '';
-//       const expected = '';
-//       const result = new Engineer('');
-//       expect(result).toEqual(expected);
-//     })
-//   })
-  
-// });
+  expect(engineer.name).toBe('Mary');
+});
+
+test("get engineer id", () => {
+  const engineer = new Engineer('Mary', 1480, 'mary@email.com', 'MaryGitHub');
+
+  expect(engineer.id).toEqual(expect.any(Number));
+});
+
+test("get engineer email", () => {
+  const engineer = new Engineer('Mary', 1480, 'mary@email.com', 'MaryGitHub');
+
+  expect(engineer.email).toStrictEqual(expect.any(String));
+});
+
+test("get engineer github name", () => {
+  const engineer = new Engineer('Mary', 1480, 'mary@email.com', 'MaryGitHub');
+
+  expect(engineer.id).toEqual(expect.any(Number));
+});
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -11,26 +11,26 @@ test('creates a manager object', () => {
 
 });
 
-// test("get manager name", () => {
-//   const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
+test("get manager name", () => {
+  const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
 
-//   expect(manager.name).toBe('Joe');
-// });
+  expect(manager.name).toBe('Joe');
+});
 
-// test("get manager id", () => {
-//   const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
+test("get manager id", () => {
+  const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
 
-//   expect(manager.id).toEqual(expect.any(Number));
-// });
+  expect(manager.id).toEqual(expect.any(Number));
+});
 
-// test("get manager email", () => {
-//   const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
+test("get manager email", () => {
+  const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
 
-//   expect(manager.email).toStrictEqual(expect.any(String));
-// });
+  expect(manager.email).toStrictEqual(expect.any(String));
+});
 
-// test("get manager office number", () => {
-//   const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
+test("get manager office number", () => {
+  const manager = new Manager('Joe', 1357, 'joe@email.com', 210-123-4567);
 
-//   expect(manager.id).toEqual(expect.any(Number));
-// });
+  expect(manager.id).toEqual(expect.any(Number));
+});
