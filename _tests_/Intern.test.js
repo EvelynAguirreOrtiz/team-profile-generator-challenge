b78c99caf732b2e4aeb174eec7235caa1ca277a1
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-04-09 17:53:23 -0500

    create intern class

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,11 +1,33 @@
 const Intern = require('../lib/Intern.js')
 
 test('creates an intern object', () => {
-  const intern = new Intern('name');
+  const intern = new Intern('Jane', 9876, 'jane@email.com', 'UTSA');
+  expect(intern.name).toBe('Jane');
+  expect(intern.id).toEqual(expect.any(Number));
+  expect(intern.email).toStrictEqual(expect.any(String));
+  expect(intern.school).toEqual(expect.any(String));
+});
 
-  // expect(engineer.name).toBe('John');
-  // expect(engineer.id).toEqual(expect.any(Number));
+test("get intern name", () => {
+  const intern = new Intern('Jane', 9876, 'jane@email.com', 'UTSA');
 
-  // expect(engineer.id).toBe(expect.any(Number));
-  // expect(engineer.email).toBe(expect.any(String));
+  expect(intern.name).toBe('Jane');
+});
+
+test("get intern id", () => {
+  const intern = new Intern('Jane', 9876, 'jane@email.com', 'UTSA');
+
+  expect(intern.id).toEqual(expect.any(Number));
+});
+
+test("get intern email", () => {
+  const intern = new Intern('Jane', 9876, 'jane@email.com', 'UTSA');
+
+  expect(intern.email).toStrictEqual(expect.any(String));
+});
+
+test("get intern github name", () => {
+  const intern = new Intern('Jane', 9876, 'jane@email.com', 'UTSA');
+
+  expect(intern.id).toEqual(expect.any(Number));
 });
