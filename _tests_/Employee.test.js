ec0ee6e5cde53e5087ba99cc4c16a058b3f60a37
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-04-09 16:05:08 -0500

    create employee class

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,46 +1,31 @@
 const Employee = require('../lib/Employee.js')
+// jest.mock('../lib/employee.js');
 
 test('creates an employee object', () => {
-  const employee = new Employee('John');
+  const employee = new Employee('John', 1234, 'email@email.com');
 
-  // expect(employee.name).toBe('John');
-  // expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.name).toBe('John');
+  expect(employee.id).toEqual(expect.any(Number));'John', 1234, 'email@email.com'
+  expect(employee.email).toStrictEqual(expect.any(String));
+});
 
-  // expect(employee.id).toBe(expect.any(Number));
-  // expect(employee.email).toBe(expect.any(String));
+test("get employee name", () => {
+  const employee = new Employee('John', 1234, 'email@email.com');
+
+  expect(employee.name).toBe('John');
+});
+
+test("get employee id", () => {
+  const employee = new Employee('John', 1234, 'email@email.com');
+
+  expect(employee.id).toEqual(expect.any(Number));
+});
+
+test("get employee email", () => {
+  const employee = new Employee('John', 1234, 'email@email.com');
+
+  expect(employee.email).toStrictEqual(expect.any(String));
 });
 
 
-describe('Employee', () => {
-  describe("get employee name", () => {
-    it('should return employee name', () => {
-      const testName = '';
-      const expected = "John";
 
-      const result = new Employee('John');
-
-      expect(result).toEqual(expected);
-      // expect(employee.getName()).toEqual('John');
-      // expect(employee.getName()).toBe('John');
-    })
-  });
-
-  // test('gets employee id', () => {
-  //   const employee = new Employee('John');
-
-  //   expect(employee.getId()).toEqual(expect.any(Number));
-  // });
-
-  // test('checks employee email', () => {
-  //   const employee = new Employee('John');
-
-  //   expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
-
-  // });
-
-  // test("gets employee role", () => {
-  //   const employee = new Employee('John');
-
-  //   expect(employee.getRole()).toEqual(expect.stringContaining(employee.role.toString()));
-  // });
-});
